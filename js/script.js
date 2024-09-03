58504e87d94b3adb6a3c01446fdeac862f2d8b95
fjgonsal31 <fjgonsal31@gmail.com>
2024-09-03 14:06:39 +0200

    030924

--- js/script.js
+++ js/script.js
@@ -1,82 +1,124 @@
-// piedra, papel o tijera
+//******************VARIABLES******************
+
 let nombreJug = prompt("¿Cómo te llamas?");
-let bPiedra = document.getElementById("piedra");
-let bPapel = document.getElementById("papel");
-let bTijera = document.getElementById("tijera");
+let btnPiedra = document.getElementById("piedra");
+let btnPapel = document.getElementById("papel");
+let btnTijera = document.getElementById("tijera");
 let pEleccion = document.getElementById("eleccion");
 let pResultado = document.getElementById("resultado");
 let pMarcador = document.getElementById("marcador");
-let botElec = [bPiedra.textContent, bPapel.textContent, bTijera.textContent];
+let botElec = [
+  btnPiedra.textContent,
+  btnPapel.textContent,
+  btnTijera.textContent,
+];
 let botElecR = null;
 let con1 = 0;
 let con2 = 0;
 
-pMarcador.textContent = `${nombreJug}: ${con1} --- BOTin: ${con2}`;
+// ******************FUNCIONES******************
 
 function reset() {
-    pEleccion.textContent = "";
-    botElecR = Math.floor(Math.random() * botElec.length);
+  pEleccion.textContent = "";
+  pResultado.textContent = "";
+  pResultado.style.border = "none";
+  botElecR = Math.floor(Math.random() * botElec.length);
 }
 
 function piedra() {
-    reset();
-    pEleccion.textContent = `${nombreJug} ha elegido: ${bPiedra.textContent} --- BOTin eligió: ${botElec[botElecR]}`;
+  reset();
 
+  setTimeout(() => {
+    pEleccion.textContent = `${nombreJug} escoge: ${btnPiedra.textContent} & BOTin eligió: ${botElec[botElecR]}`;
+  }, 400);
+
+  setTimeout(() => {
     switch (botElec[botElecR]) {
-        case bPiedra.textContent:
-            pResultado.textContent = "¡EMPATE!";
-            break;
-        case bPapel.textContent:
-            pResultado.textContent = "¡PERDISTE!";
-            con2++;
-            break;
-        case bTijera.textContent:
-            pResultado.textContent = "¡GANASTE!";
-            con1++;
-            break;
+      case btnPiedra.textContent:
+        pResultado.textContent = "¡EMPATE! 😐";
+        pResultado.style.border = "3px solid black";
+        break;
+      case btnPapel.textContent:
+        pResultado.textContent = "¡PERDISTE! 😭";
+        pResultado.style.border = "3px solid red";
+        con2++;
+        break;
+      case btnTijera.textContent:
+        pResultado.textContent = "¡GANASTE! 😎";
+        pResultado.style.border = "3px solid green";
+        con1++;
+        break;
     }
-
-    pMarcador.textContent = `${nombreJug}: ${con1} --- BOTin: ${con2}`;
+    showMarcador(con1, con2);
+  }, 1300);
 }
 
 function papel() {
-    reset();
-    pEleccion.textContent = `${nombreJug} ha elegido: ${bPapel.textContent} --- BOTin eligió: ${botElec[botElecR]}`;
+  reset();
 
+  setTimeout(() => {
+    pEleccion.textContent = `${nombreJug} escoge: ${btnPapel.textContent} & BOTin eligió: ${botElec[botElecR]}`;
+  }, 400);
+
+  setTimeout(() => {
     switch (botElec[botElecR]) {
-        case bPiedra.textContent:
-            pResultado.textContent = "¡GANASTE!";
-            con1++;
-            break;
-        case bPapel.textContent:
-            pResultado.textContent = "¡EMPATE!";
-            break;
-        case bTijera.textContent:
-            pResultado.textContent = "¡PERDISTE!";
-            con2++;
-            break;
+      case btnPiedra.textContent:
+        pResultado.textContent = "¡GANASTE! 😎";
+        pResultado.style.border = "3px solid green";
+        con1++;
+        break;
+      case btnPapel.textContent:
+        pResultado.textContent = "¡EMPATE! 😐";
+        pResultado.style.border = "3px solid black";
+        break;
+      case btnTijera.textContent:
+        pResultado.textContent = "¡PERDISTE! 😭";
+        pResultado.style.border = "3px solid red";
+        con2++;
+        break;
     }
-
-    pMarcador.textContent = `${nombreJug}: ${con1} --- BOTin: ${con2}`;
+    showMarcador(con1, con2);
+  }, 1300);
 }
 
 function tijera() {
-    reset();
-    pEleccion.textContent = `${nombreJug} ha elegido: ${bTijera.textContent} --- BOTin eligió: ${botElec[botElecR]}`;
+  reset();
 
+  setTimeout(() => {
+    pEleccion.textContent = `${nombreJug} escoge: ${btnTijera.textContent} & BOTin eligió: ${botElec[botElecR]}`;
+  }, 400);
+
+  setTimeout(() => {
     switch (botElec[botElecR]) {
-        case bPiedra.textContent:
-            pResultado.textContent = "¡PERDISTE!";
-            con2++;
-            break;
-        case bPapel.textContent:
-            pResultado.textContent = "¡GANASTE!";
-            con1++;
-            break;
-        case bTijera.textContent:
-            pResultado.textContent = "¡EMPATE!";
-            break;
+      case btnPiedra.textContent:
+        pResultado.textContent = "¡PERDISTE! 😭";
+        pResultado.style.border = "3px solid red";
+        con2++;
+        break;
+      case btnPapel.textContent:
+        pResultado.textContent = "¡GANASTE! 😎";
+        pResultado.style.border = "3px solid green";
+        con1++;
+        break;
+      case btnTijera.textContent:
+        pResultado.textContent = "¡EMPATE! 😐";
+        pResultado.style.border = "3px solid black";
+        break;
     }
-
-    pMarcador.textContent = `${nombreJug}: ${con1} --- BOTin: ${con2}`;
+    showMarcador(con1, con2);
+  }, 1300);
 }
+
+function showMarcador(con1, con2) {
+  pMarcador.textContent = `${nombreJug}: ${con1} - BOTin: ${con2}`;
+}
+
+//******************EJECUCION******************
+
+if (nombreJug) {
+  nombreJug = nombreJug.slice(0, 20);
+} else {
+  nombreJug = "User";
+}
+
+showMarcador(con1, con2);
